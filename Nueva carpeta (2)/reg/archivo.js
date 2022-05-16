a5aff8ac99a1e5f0ec8fdec03bc7926ec2476321
Nidiade09 <101573910+Nidiade09@users.noreply.github.com>
2022-05-16 01:10:12 -0500

    Add files via upload

---
+++ Nueva carpeta (2)/reg/archivo.js
@@ -0,0 +1,9 @@
+const dos_tomos = 2;
+const un_tomo = 2; 
+const reg_untomo= 1;
+const reg_dostomos=2;
+
+const cantidad_tomos = document.getElementById(cantidad)
+const cantidad_regalos = document.getElementById(regalos)
+const indicador = document.querySelector("")
+
